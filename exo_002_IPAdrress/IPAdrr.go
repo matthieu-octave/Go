60bbd5ae559611e617e1c9c4a1d4f4637dfbf005
Matthieu OCTAVE <matthieu.octave@gmail.com>
2024-04-05 09:42:10 +0200

    -m up

--- exo_002_IPAdrress/IPAdrr.go
+++ exo_002_IPAdrress/IPAdrr.go
@@ -8,8 +8,7 @@ func (ip IPAddr) String() string {
 	return fmt.Sprintf("%v.%v.%v.%v", ip[0], ip[1], ip[2], ip[3])
 }
 
-// TODO: Add a "String() string" method to IPAddr.
-
+// TODO: Add a functionnality to get my IP and show it well formatted
 func main() {
 	hosts := map[string]IPAddr{
 		"loopback":  {127, 0, 0, 1},
